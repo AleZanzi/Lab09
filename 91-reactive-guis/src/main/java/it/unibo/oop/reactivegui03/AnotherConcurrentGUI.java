a0406b431ca1ddfd27367e1edc920329f04c4705
Alessandro Zanzi <alessandro.zanzi2@studio.unibo.it>
2022-11-22 16:05:04 +0100

    es 91 solved

--- 91-reactive-guis/src/main/java/it/unibo/oop/reactivegui03/AnotherConcurrentGUI.java
+++ 91-reactive-guis/src/main/java/it/unibo/oop/reactivegui03/AnotherConcurrentGUI.java
@@ -17,4 +17,96 @@ import javax.swing.SwingUtilities;
  * Third experiment with reactive gui.
  */
 public final class AnotherConcurrentGUI extends JFrame {
+    private static final long serialVersionUID = 1L;
+    private static final double WIDTH_PERC = 0.2;
+    private static final double HEIGHT_PERC = 0.1;
+    protected static final long WAITING_TIME = TimeUnit.SECONDS.toMillis(10);
+    private final JLabel display = new JLabel();
+    private final JButton stop = new JButton("stop");
+    private final JButton up = new JButton("up");
+    private final JButton down = new JButton("down");
+
+    private final Agent agent = new Agent();
+
+
+    /**
+     * Builds a new CGUI.
+     */
+    public AnotherConcurrentGUI() {
+        super();
+        final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setSize((int) (screenSize.getWidth() * WIDTH_PERC), (int) (screenSize.getHeight() * HEIGHT_PERC));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        final JPanel panel = new JPanel();
+        panel.add(display);
+        panel.add(up);
+        panel.add(down);
+        panel.add(stop);
+        this.getContentPane().add(panel);
+        this.setVisible(true);
+
+        up.addActionListener((e) -> agent.upCounting());
+        down.addActionListener((e) -> agent.downCounting());
+        stop.addActionListener((e) -> agent.stopCounting());
+
+        new Thread(agent).start();
+        new Thread(() -> {
+            try {
+                Thread.sleep(WAITING_TIME);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            this.stopCounting();
+        }).start();
+    }
+
+    private void stopCounting() {
+        agent.stopCounting();
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                stop.setEnabled(false);
+                up.setEnabled(false);
+                down.setEnabled(false);
+            }
+        });
+    }
+
+    private class Agent implements Runnable {
+
+        private volatile boolean stop;
+        private volatile boolean normalCount = true;
+        private int counter;
+
+        @Override
+        public void run() {
+            while (!this.stop) {
+                try {
+                    // The EDT doesn't access `counter` anymore, it doesn't need to be volatile 
+                    final var nextText = Integer.toString(this.counter);
+                    SwingUtilities.invokeAndWait(() -> AnotherConcurrentGUI.this.display.setText(nextText));
+                    if (normalCount) {
+                        this.counter++;
+                    } else {
+                        this.counter--;
+                    }
+                    Thread.sleep(100);
+                } catch (InvocationTargetException | InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+        public void downCounting() {
+            this.normalCount = false;
+        }
+
+        public void upCounting() {
+            this.normalCount = true;
+        }
+
+        public void stopCounting() {
+            this.stop = true;
+        }
+    }
 }
